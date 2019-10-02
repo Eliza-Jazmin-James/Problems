b9feb2140c2460645bf800a8fad10a639b51bef5
Eliza-Jazmin-James <56062868+Eliza-Jazmin-James@users.noreply.github.com>
2019-10-02 11:53:28 +0530

    Add files via upload

---
+++ ExecutionClass.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace SplitAndSort
+{
+	public static class ExecutionClass
+	{
+		public static void WithoutRegex(string text, string stringPattern, ArrayList finalList)
+		{
+			int textlength = text.Length;
+			int patternLength = stringPattern.Length;
+			if (patternLength > textlength)
+			{
+				return;
+			}
+			string emailId = string.Empty;
+			string firstname = string.Empty;
+			char[] characterArray = text.ToCharArray();
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (characterArray[i] >= 'a' && characterArray[i] <= 'z')
+				{
+					continue;
+				}
+				else if (characterArray[i] == ' ')
+				{
+					int spaceIndex = i;
+					emailId = text.Substring(spaceIndex + 1);
+					firstname = text.Substring(0, spaceIndex);
+					if (firstname.Length > 20)
+					{
+						return;
+					}
+					if (emailId.Length > 50)
+					{
+						return;
+					}
+				}
+				else if (characterArray[i] == '@')
+				{
+					if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(emailId))
+					{
+						return;
+					}
+					string subString = text.Substring(i);
+					if (subString == stringPattern)
+					{
+
+						finalList.Add(firstname);
+						return;
+
+					}
+					else
+					{
+						return;
+					}
+				}
+				else
+				{
+					return;
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace SplitAndSort
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string stringPattern = "@gmail.com";
+			ArrayList finalList = new ArrayList(30);
+			Console.WriteLine("Enter the number of inputs");
+			int numberOfData = Convert.ToInt32(Console.ReadLine());
+			string[] stringarr = new string[30];
+			for (int i = 0; i < numberOfData; i++)
+			{
+				stringarr[i] = Console.ReadLine();
+			}
+
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			for (int l = 0; l < numberOfData; l++)
+			{
+				ExecutionClass.WithoutRegex(stringarr[l], stringPattern, finalList);
+			}
+			finalList.Sort();
+			stopWatch.Stop();
+			Console.WriteLine("\n\n\nOUTPUT");
+			foreach (var output in finalList)
+			{
+				Console.WriteLine(output);
+			}
+			Console.WriteLine("Time elapsed (ms) for Without Regex: {0}", stopWatch.Elapsed.TotalMilliseconds);
+			Console.ReadLine();
+		}
+
+		
+	}
+}
